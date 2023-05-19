41fa4a7360247605900caa3c1903f574a70efecb
ckgudcjf <ckgudcjf@gmail.com>
2023-05-19 10:24:34 +0900

    ing

--- bnc.py
+++ bnc.py
@@ -64,8 +64,9 @@ class BotBinance():
         self.b_l = list(set(self.q_l + bal_lst))
         self.r_l = list(set(bal_lst).difference(self.q_l))
         self.prc_ttl = prc_ttl if prc_ttl < self.const_up else self.const_up
+        self.prc_ttl = 3750
         self.prc_lmt = prc_lmt if prc_ttl < self.const_up else prc_lmt - (prc_ttl - self.const_up)
-        prc_buy = self.prc_ttl / (len(self.q_l) * 4)
+        prc_buy = self.prc_ttl / (len(self.q_l) * 3)
         self.prc_buy = prc_buy if prc_buy > self.const_dn else self.const_dn
 
         if os.path.isfile(FILE_URL_TIKR_3M):
@@ -276,7 +277,7 @@ class BotBinance():
 
         
         lst = sorted(lst, key=lambda x: x['v'])
-        lst = lst[-55:]
+        lst = lst[-40:]
         lst = [t['t'] for t in lst]
 
         return lst
