99d64181cbe73dcfc43c72d27b63d8c2f36e18a2
ckgudcjf <ckgudcjf@gmail.com>
2023-05-23 12:55:39 +0900

    ing

--- bnc.py
+++ bnc.py
@@ -14,7 +14,7 @@ class BotBinance():
 
     def __init__(self):
 
-        self.is_aws = False
+        self.is_aws = True
         self.access_key = BN_ACCESS_KEY_AWS if self.is_aws else BN_ACCESS_KEY_NAJU
         self.secret_key = BN_SECRET_KEY_AWS if self.is_aws else BN_SECRET_KEY_NAJU
         self.bnc = ccxt.binance(config={'apiKey': self.access_key, 'secret': self.secret_key, 'enableRateLimit': True})
@@ -36,7 +36,7 @@ class BotBinance():
         self.prc_lmt = 0
         self.prc_buy = 0
 
-        self.const_up = 305000
+        self.const_up = 375000
         self.const_dn = 12.5
 
 
