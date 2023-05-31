cf2ea84dcdbd99f74a419adf9759b2a1b045074b
ckgudcjf <ckgudcjf@gmail.com>
2023-05-31 20:56:22 +0900

    ing

--- bnc.py
+++ bnc.py
@@ -169,7 +169,7 @@ class BotBinance():
         self.b_l = list(set(self.q_l + bal_lst))
         self.r_l = list(set(bal_lst).difference(self.q_l))
         self.prc_ttl = prc_ttl if prc_ttl < self.const_up else self.const_up
-        self.prc_ttl = 18000
+        self.prc_ttl = 20000
         self.prc_lmt = prc_lmt if prc_ttl < self.const_up else prc_lmt - (prc_ttl - self.const_up)
         prc_buy = self.prc_ttl / 800
         self.prc_buy = prc_buy if prc_buy > self.const_dn else self.const_dn
