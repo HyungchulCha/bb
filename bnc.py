23e2b7d32549d52a80e3687ef29c736af9c86cf0
ckgudcjf <ckgudcjf@gmail.com>
2023-06-02 14:46:47 +0900

    ing

--- bnc.py
+++ bnc.py
@@ -40,101 +40,6 @@ class BotBinance():
         self.const_up = 375000
         self.const_dn = 11
 
-
-    def top_tier(self):
-
-        sym_arr = []
-        pft_arr = []
-        obj_lst = {}
-        bal_obj = {}
-        tks = self.get_filter_ticker(True)
-
-        for code in tks:
-
-            df = self.strategy_rsi(self.gen_bnc_df(code, '5m', 12*24*3))
-
-            obj_lst[code] = {
-                'bool_buy': False,
-                'buy_price': 0,
-                'quantity_ratio': 0,
-                'bool_sell': False,
-                '70_position': ''
-            }
-            
-            if not df is None:
-                _ror = 1
-                bal_obj[code] = {'b': 0}
-
-                for i, row in df.iterrows():
-                    
-                    rsi = row['rsi']
-                    rsi_prv = row['rsi_prev']
-                    vol_osc = row['volume_osc']
-                    cur_prc = float(row['close'])
-
-                    if (rsi <= 30) and (rsi_prv > rsi) and (vol_osc > 0):
-
-                        bb = copy.deepcopy(obj_lst[code]['bool_buy'])
-
-                        if bb:
-                            bp = copy.deepcopy(obj_lst[code]['buy_price'])
-                            qr = copy.deepcopy(obj_lst[code]['quantity_ratio'])
-                            obj_lst[code]['buy_price'] = ((bp * (qr - 1)) + cur_prc) / qr
-                            obj_lst[code]['quantity_ratio'] = qr + 1
-                        else:
-                            obj_lst[code] = {
-                                'bool_buy': True,
-                                'buy_price': cur_prc,
-                                'quantity_ratio': 2,
-                                'bool_sell': False,
-                                '70_position': ''
-                            }
-
-                    if obj_lst[code]['bool_buy'] == True:
-
-                        bp = float(copy.deepcopy(obj_lst[code]['buy_price']))
-                        qr = copy.deepcopy(obj_lst[code]['quantity_ratio'])
-                        bs = copy.deepcopy(obj_lst[code]['bool_sell'])
-                        p7 = copy.deepcopy(obj_lst[code]['70_position'])
-
-                        if rsi <= 50 and bs:
-                            _ror = get_ror(bp, cur_prc, _ror)
-                            obj_lst[code] = {
-                                'bool_buy': False,
-                                'buy_price': 0,
-                                'quantity_ratio': 0,
-                                'bool_sell': False,
-                                '70_position': ''
-                            }
-
-                        elif rsi >= 70 and ((p7 == '70_down') or (p7 == '70_up' and (rsi_prv <= rsi))):
-                            _ror = get_ror(bp, cur_prc, _ror)
-                            obj_lst[code]['quantity_ratio'] = qr - 1
-                            obj_lst[code]['bool_sell'] = True
-
-                            if obj_lst[code]['quantity_ratio'] == 0:
-                                obj_lst[code] = {
-                                    'bool_buy': False,
-                                    'buy_price': 0,
-                                    'quantity_ratio': 0,
-                                    'bool_sell': False,
-                                    '70_position': ''
-                                }
-
-                    obj_lst[code]['70_position'] = '70_down' if rsi < 70 else '70_up'
-
-                pft_per = round(((_ror - 1) * 100), 2)
-
-                sym_arr.append(code)
-                pft_arr.append(pft_per)
-
-        pft_df = pd.DataFrame({'code': sym_arr, 'profit': pft_arr})
-        pft_df = pft_df.loc[(pft_df['profit'] > 0)]
-        pft_df = pft_df.sort_values('profit', ascending=False)
-        print(pft_df)
-        ttr_li = pft_df.head(80)['code'].to_list()
-        save_file(FILE_URL_TPTR_3M, ttr_li)
-
     
     def init_per_day(self):
 
@@ -151,13 +56,6 @@ class BotBinance():
                 time.sleep(300 - tn_d + 150)
 
             self.bool_balance = True
-
-        # tn_tt = datetime.datetime.now()
-
-        # if ((tn_tt.hour % 3) == 2) and tn_tt.minute == 57 and (30 <= tn_tt.second < 35):
-        #     self.top_tier()
-        #     self.t_l = load_file(FILE_URL_TPTR_3M)
-        #     line_message(f'BotBinance \nToday Top-Tier \n{self.t_l}')
 
         print('##############################')
 
@@ -196,6 +94,8 @@ class BotBinance():
         int_prc_lmt = int(self.prc_lmt)
         len_qnt_lst = len(self.q_l)
 
+        print(self.r_l)
+
         line_message(f'BotBinance \nT : {int_prc_ttl:,} USDT \nR : {int_rel_ttl:,} USDT \nL : {int_prc_lmt:,} USDT \nS : {len_qnt_lst}')
 
         __tn = datetime.datetime.now()
@@ -222,7 +122,6 @@ class BotBinance():
 
         _, _, bal_lst, _ = self.get_balance_info()
         sel_lst = []
-        cur_pft = []
 
         for symbol in self.b_l:
 
@@ -246,12 +145,6 @@ class BotBinance():
                 psb_sel = (bal_sym and (cur_prc * bal_lst[symbol]['b'] > self.const_dn))
                 bb = copy.deepcopy(self.o_l[symbol]['bool_buy'])
                 nt = bb and ((not bal_sym) or (bal_sym and (cur_prc * bal_lst[symbol]['b'] < self.const_dn)))
-
-                if bb:
-                    _bp = copy.deepcopy(self.o_l[symbol]['buy_price'])
-                    _cur_ror = get_ror(_bp, cur_prc)
-                    _cur_ror = round(_cur_ror, 4)
-                    cur_pft.append(f'{symbol} : {_cur_ror}')
 
                 if nt:
                     self.get_tiker_data_init(symbol)
@@ -339,15 +232,6 @@ class BotBinance():
         for sl in sel_lst:
             sel_txt = sel_txt + '\n' + str(sl['c']) + ' : ' + str(sl['r'])
 
-        cur_pft_frst = True
-        cur_pft_text = ''
-        for cp in cur_pft:
-            if cur_pft_frst:
-                cur_pft_text = cur_pft_text + '\n' + cp
-                cur_pft_frst = False
-            else:
-                cur_pft_text = cur_pft_text + ' / ' + cp
-
         __tn = datetime.datetime.now()
         __tn_min = __tn.minute % 5
         __tn_sec = __tn.second
@@ -358,7 +242,6 @@ class BotBinance():
         str_start = _tn.strftime('%Y/%m/%d %H:%M:%S')
         str_end = __tn.strftime('%Y/%m/%d %H:%M:%S')
 
-        # line_message(f'BotBinance \nS : {str_start} \nE : {str_end} {cur_pft_text} {sel_txt}')
         line_message(f'BotBinance \nS : {str_start} \nE : {str_end} {sel_txt}')
     
 
@@ -390,18 +273,7 @@ class BotBinance():
                 _tks = self.bnc.fetch_ticker(mk)
                 if float(_tks['info']['priceChangePercent']) > 0:
                     tks.append({'t': mk, 'c': float(_tks['info']['priceChangePercent'])})
-
-        #         if not init:
-        #             if mk in self.t_l:
-        #                 tks.append(mk)
-        #         else:
-                # tks.append(mk)
-
-        # return tks
-
-        # if len(tks) > 100:
-        #     _lst = sorted(tks, key=lambda t: t['c'])[-100:]
-        # else:
+                    
         _lst = sorted(tks, key=lambda t: t['c'])[::-1]
         lst = [l['t'] for l in _lst]
 
